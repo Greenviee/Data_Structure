868289a8fe54e6e4940e5c2e7f3bcacc7245653b
Jinwoo Kim <131782814+Greenviee@users.noreply.github.com>
2023-10-12 09:42:24 +0900

    Update integer_linked_list.cpp

--- 4장/integer_linked_list.cpp
+++ 4장/integer_linked_list.cpp
@@ -2,6 +2,7 @@
 1단계-정수 연결 리스트: 단순한 linked list에서 insert, delete하는 알고리즘을 코딩
 */
 #include <iostream>
+#include <time.h>
 using namespace std;
 
 class Node {
@@ -29,7 +30,23 @@ public:
 	void Show();
 	void Add(int element);
 	bool Search(int data);
+	LinkedList& operator+ (LinkedList&);
 };
+
+LinkedList& LinkedList::operator+ (LinkedList& lb) {
+	LinkedList lc;
+	Node* a = first; Node* b = lb.first;
+	while (a != nullptr) {
+		lc.Add(a->data);
+		a = a->link;
+	}
+	while (b != nullptr) {
+		lc.Add(b->data);
+		b = b->link;
+	}
+	return lc;
+}
+
 void LinkedList::Show() { // 전체 리스트를 순서대로 출력한다.
 	Node* p = first;
 	if (first == nullptr) {
@@ -66,7 +83,7 @@ void LinkedList::Add(int element) // 임의 값을 삽입할 때 리스트가 오름차순으로 �
 		p->link = newNode;
 	}
 }
-bool LinkedList::Search(int data) { 
+bool LinkedList::Search(int data) {
 	Node* p = first;
 	while (p != nullptr) {
 		if (p->data == data) return true;
@@ -96,45 +113,56 @@ bool LinkedList::Delete(int element) // delete the element
 }
 
 enum Enum {
-	Add, Delete, Show, Search, Exit
+	Add0, Add1, Delete, Show, Search, Merge, Exit
 };
 int main() {
 	Enum menu; // 메뉴
 	int selectMenu;
 	int num = 0; bool result = false;
 	srand(time(NULL));
-	LinkedList* l = new LinkedList();
+	LinkedList la, lb, lc;
 
 	int data = 0;
 	//l->Show();
 	do {
-		cout << "0.ADD, 1.Delete, 2.Show, 3.Search, 4. Exit" << endl << "선택::";
+		cout << "0.ADD0, 1. ADD1, 2.Delete, 3.Show, 4.Search, 5. Merge, 6. Exit" << endl << "선택::";
 		cin >> selectMenu;
 		switch (static_cast<Enum>(selectMenu)) {
-		case Add: // 머리노드 삽입
+		case Add0: // 머리노드 삽입
 			data = rand() % 49;
-			l->Add(data);
+			la.Add(data);
 			cout << "삽입후 리스트: ";
-			l->Show();
+			la.Show();
+			break;
+		case Add1: // 머리노드 삽입
+			data = rand() % 49;
+			lb.Add(data);
+			cout << "삽입후 리스트: ";
+			lb.Show();
 			break;
 		case Delete: // 머리 노드 삭제
 			cin >> data;
-			result = l->Delete(data);
+			result = la.Delete(data);
 			if (result)
 				cout << "삭제된 데이터는 " << data << endl;
 			break;
 		case Show: // 꼬리 노드 삭제
-			l->Show();
+			la.Show();
 			break;
 		case Search: // 회원 번호 검색
 			int n; cin >> n;
-			result = l->Search(n);
+			result = la.Search(n);
 			if (!result)
 				cout << "검색 값 = " << n << " 데이터가 없습니다." << endl;
 			else
 				cout << "검색 값 = " << n << " 데이터가 존재합니다." << endl;
 			break;
-		case Exit: 
+		case Merge:
+			lc = la + lb;
+			cout << "합친 후 리스트: ";
+			lc.Show();
+			break;
+		case Exit:
 			cout << "Exit" << endl;
 			exit(1);
 			break;
