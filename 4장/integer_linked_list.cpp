2d4031bb066550faf4352be9348e236f1ac6c239
Jinwoo Kim <a01047463862@gmail.com>
2023-10-17 21:41:48 +0900

    Update integer_linked_list.cpp

--- 4장/integer_linked_list.cpp
+++ 4장/integer_linked_list.cpp
@@ -1,5 +1,5 @@
 /*
-1단계-정수 연결 리스트: 단순한 linked list에서 insert, delete하는 알고리즘을 코딩
+1단계-정수 연결 리스트: 단순한 linked list에서 add, delete하는 알고리즘을 코딩
 */
 #include <iostream>
 #include <time.h>
@@ -14,10 +14,6 @@ public:
 		data = element;
 		link = nullptr;
 	}
-	friend ostream& operator<< (ostream& os, Node& n) {
-		os << n.data << " ";
-		return os;
-	}
 };
 
 class LinkedList {
@@ -28,14 +24,94 @@ public:
 	}
 	bool Delete(int);
 	void Show();
-	void Add(int element);
+	void Add(int element);//정렬되도록 구현
 	bool Search(int data);
-	LinkedList& operator+ (LinkedList&);
+	LinkedList& operator+(LinkedList&);
 };
+void LinkedList::Show() { // 전체 리스트를 순서대로 출력한다.
+	Node* p = first;
+	if (p == nullptr) {
+		cout << "Empty List" << endl;
+		return;
+	}
+	while (p != nullptr) {
+		cout << p->data << ' ';
+		p = p->link;
+	}
+	cout << endl;
+	return;
+}
 
-LinkedList& LinkedList::operator+ (LinkedList& lb) {
+void LinkedList::Add(int element) // 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
+{
+	Node* newNode = new Node(element);
+	Node* p = first, * q = nullptr;
+	if (p == nullptr) { //연결 리스트가 비어있는 경우
+		first = newNode;
+		return;
+	}
+	if (newNode->data < p->data) { //맨 앞에 새 노드를 추가하는 경우
+		newNode->link = p;
+		first = newNode;
+		return;
+	}
+	while (p != nullptr) {
+		if (newNode->data < p->data) {
+			newNode->link = p;
+			q->link = newNode;
+			return;
+		}
+		q = p;
+		p = p->link;
+	}
+	//맨 끝에 새 노드를 추가하는 경우
+	q->link = newNode;
+	return;
+}
+
+bool LinkedList::Search(int data) {
+	Node* p = first;
+	while (p != nullptr) {
+		if (p->data == data)
+			return true;
+		p = p->link;
+	}
+	return false;
+}
+
+bool LinkedList::Delete(int element) // delete the element
+{
+	Node* p = first, * q = nullptr;
+	while (p != nullptr) {
+		if (p->data == element) {
+			if (p == first) { //지워야 하는 노드가 first인 경우
+				first = p->link;
+				delete p;
+				return true;
+			}
+			q->link = p->link;
+			delete p;
+			return true;
+		}
+		q = p;
+		p = p->link;
+	}
+	return true;
+}
+
+LinkedList& LinkedList::operator+(LinkedList& lb) {
 	LinkedList lc;
-	Node* a = first; Node* b = lb.first;
+	Node* a = first, * b = lb.first;
+	while (a != nullptr && b != nullptr) {
+		if (a->data < b->data) {
+			lc.Add(a->data);
+			a = a->link;
+		}
+		else {
+			lc.Add(b->data);
+			b = b->link;
+		}
+	}
 	while (a != nullptr) {
 		lc.Add(a->data);
 		a = a->link;
@@ -47,107 +123,39 @@ LinkedList& LinkedList::operator+ (LinkedList& lb) {
 	return lc;
 }
 
-void LinkedList::Show() { // 전체 리스트를 순서대로 출력한다.
-	Node* p = first;
-	if (first == nullptr) {
-		cout << "Empty List" << endl;
-		return;
-	}
-	while (p != nullptr) {
-		cout << p->data << " ";
-		p = p->link;
-	}
-	cout << endl;
-}
-void LinkedList::Add(int element) // 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
-{
-	Node* newNode = new Node(element);
-	if (first == nullptr) { //linked list가 비어있는 경우
-		first = newNode;
-		return;
-	}
-	Node* p = first;
-	if (p->data > element) {
-		newNode->link = p;
-		first = newNode;
-	}
-	else {
-		while (p->link != nullptr) {
-			if (p->link->data > element) {
-				newNode->link = p->link;
-				p->link = newNode;
-				return;
-			}
-			else p = p->link;
-		}
-		p->link = newNode;
-	}
-}
-bool LinkedList::Search(int data) {
-	Node* p = first;
-	while (p != nullptr) {
-		if (p->data == data) return true;
-		p = p->link;
-	}
-	return false;
-}
-bool LinkedList::Delete(int element) // delete the element
-{
-	Node* p = first;
-	Node* prev = nullptr;
-	while (p != nullptr) {
-		if (p->data == element) {
-			if (prev == nullptr) { //삭제할 노드가 첫번째 노드인 경우
-				first = p->link;
-			}
-			else {
-				prev->link = p->link;
-			}
-			delete p;
-			return true;
-		}
-		prev = p;
-		p = p->link;
-	}
-	return false;// 삭제할 대상이 없다.
-}
-
 enum Enum {
-	Add0, Add1, Delete, Show, Search, Merge, Exit
+	Add1, Add2, Delete, Show, Search, Merge, Exit
 };
-int main() {
+void main() {
 	Enum menu; // 메뉴
 	int selectMenu;
 	int num = 0; bool result = false;
 	srand(time(NULL));
 	LinkedList la, lb, lc;
-
 	int data = 0;
-	//l->Show();
 	do {
-		cout << "0.ADD0, 1. ADD1, 2.Delete, 3.Show, 4.Search, 5. Merge, 6. Exit" << endl << "선택::";
+		cout << "0.Add0, 1. Add1, 2.Delete, 3.Show, 4.Search, 5. Merge, 6. Exit" << endl << "선택::";
 		cin >> selectMenu;
 		switch (static_cast<Enum>(selectMenu)) {
-		case Add0: // 머리노드 삽입
+		case Add1:
 			data = rand() % 49;
 			la.Add(data);
-			cout << "삽입후 리스트: ";
-			la.Show();
 			break;
-		case Add1: // 머리노드 삽입
+		case Add2:
 			data = rand() % 49;
 			lb.Add(data);
-			cout << "삽입후 리스트: ";
-			lb.Show();
 			break;
-		case Delete: // 머리 노드 삭제
+		case Delete:
 			cin >> data;
 			result = la.Delete(data);
 			if (result)
-				cout << "삭제된 데이터는 " << data << endl;
+				cout << "삭제 완료" << endl;
 			break;
-		case Show: // 꼬리 노드 삭제
+		case Show:
+			cout << "리스트 la = ";
 			la.Show();
+			cout << "리스트 lb = ";
+			lb.Show();
 			break;
 		case Search: // 회원 번호 검색
 			int n; cin >> n;
@@ -159,7 +167,7 @@ int main() {
 			break;
 		case Merge:
 			lc = la + lb;
-			cout << "합친 후 리스트: ";
+			cout << "리스트 lc = ";
 			lc.Show();
 			break;
 		case Exit:
