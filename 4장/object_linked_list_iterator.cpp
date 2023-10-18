459e1aad6ff58d5ff380c6e83221b8f24acfbbe8
Jinwoo Kim <a01047463862@gmail.com>
2023-10-18 21:12:00 +0900

    Update object_linked_list_iterator.cpp

--- 4장/object_linked_list_iterator.cpp
+++ 4장/object_linked_list_iterator.cpp
@@ -16,35 +16,60 @@ private:
 public:
 	Employee() {}
 	Employee(string sno, string sname, int salary) :eno(sno), ename(sname), salary(salary) {}
-	friend ostream& operator<<(ostream& os, Employee& e) {
-		os << "No: " << e.eno << " Name: " << e.ename << " Salary: " << e.salary << endl;
-		return os;
-	}
-	bool operator<(Employee& e) {
-		if (stoi(eno) < stoi(e.eno)) return true;
-		return false;
-	}
-	bool operator==(Employee& e) {
-		if (eno == e.eno && ename == e.ename && salary == e.salary) return true;
-		return false;
-	}
+	friend ostream& operator<<(ostream& os, Employee&);
+	bool operator<(Employee&);
+	bool operator==(Employee&);
+	char compare(const Employee*)const;
 	int getSalary() const {
 		return salary;
 	}
 };
-
+ostream& operator<<(ostream& os, Employee& emp) {
+	os << "No: " << emp.eno << " Name: " << emp.ename << " Salary: " << emp.salary << endl;
+	return os;
+}
+bool Employee::operator==(Employee& emp) {
+	if (eno == emp.eno && ename == emp.ename && salary == emp.salary)
+		return true;
+	return false;
+}
+bool Employee::operator<(Employee& emp) {
+	if (stoi(eno) < stoi(emp.eno))
+		return true;
+	return false;
+}
+char Employee::compare(const Employee* emp) const {
+	if (stoi(eno) < stoi(emp->eno))
+		return '<';
+	else if (stoi(eno) > stoi(emp->eno))
+		return '>';
+	else {
+		if (ename < emp->ename)
+			return '>';
+		else if (ename < emp->ename)
+			return '<';
+		else {
+			if (salary < emp->salary)
+				return '<';
+			else if (salary > emp->salary)
+				return '>';
+			else return '=';
+		}
+	}
+}
 class Node {
 	friend class ListIterator;
 	friend class LinkedList;
 	Employee data;
 	Node* link;
 public:
+	Node() {}
 	Node(Employee element) {
 		data = element;
 		link = nullptr;
 	}
 };
-
+class ListIterator;
 class LinkedList {
 	friend class ListIterator;
 	Node* first;
@@ -56,71 +81,8 @@ public:
 	void Show();
 	void Add(Employee);//sno로 정렬되도록 구현
 	bool Search(string);
+	LinkedList& operator+(LinkedList&);
 };
-void LinkedList::Show() { // 전체 리스트를 순서대로 출력한다.
-	Node* p = first;
-	if (first == nullptr) {
-		cout << "Empty List" << endl;
-		return;
-	}
-	while (p != nullptr) {
-		cout << p->data;
-		p = p->link;
-	}
-	cout << endl;
-}
-void LinkedList::Add(Employee element) // 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
-{
-	Node* newNode = new Node(element);
-	if (first == nullptr) { //linked list가 비어있는 경우
-		first = newNode;
-		return;
-	}
-	Node* p = first;
-	if (element < p->data) {
-		newNode->link = p;
-		first = newNode;
-	}
-	else {
-		while (p->link != nullptr) {
-			if (element < p->link->data) {
-				newNode->link = p->link;
-				p->link = newNode;
-				return;
-			}
-			else p = p->link;
-		}
-		p->link = newNode;
-	}
-}
-bool LinkedList::Search(string eno) { // sno를 갖는 레코드를 찾기
-	Node* p = first;
-	while (p != nullptr) {
-		if (p->data.eno == eno) return true;
-		p = p->link;
-	}
-	return false;
-}
-bool LinkedList::Delete(string eno) // delete the element
-{
-	Node* p = first;
-	Node* prev = nullptr;
-	while (p != nullptr) {
-		if (p->data.eno == eno) {
-			if (prev == nullptr) { //삭제할 노드가 첫번째 노드인 경우
-				first = p->link;
-			}
-			else {
-				prev->link = p->link;
-			}
-			delete p;
-			return true;
-		}
-		prev = p;
-		p = p->link;
-	}
-	return false;// 삭제할 대상이 없다.
-}
 class ListIterator {
 public:
 	ListIterator(const LinkedList& lst);
@@ -143,24 +105,106 @@ private:
 ListIterator::ListIterator(const LinkedList& lst) : list(lst), current(lst.first) {
 	cout << "List Iterator is constructed" << endl;
 }
+void LinkedList::Show() { // 전체 리스트를 순서대로 출력한다.
+	Node* p = first;
+	while (p != nullptr) {
+		cout << p->data;
+		p = p->link;
+	}
+}
+void LinkedList::Add(Employee element) // 임의 값을 삽입할 때 리스트가 오름차순으로 정렬이 되도록 한다
+{
+	Node* newNode = new Node(element);
+	Node* p = first, * q = nullptr;
+	if (p == nullptr) {
+		first = newNode;
+		return;
+	}
+	while (p != nullptr) {
+		if (element < p->data) {
+			if (p == first) {
+				newNode->link = p;
+				first = newNode;
+				return;
+			}
+			newNode->link = p;
+			q->link = newNode;
+			return;
+		}
+		q = p;
+		p = p->link;
+	}
+	q->link = newNode;
+	return;
+}
+bool LinkedList::Search(string eno) { // sno를 갖는 레코드를 찾기
+	Node* p = first;
+	while (p != nullptr) {
+		if (p->data.ename == eno)
+			return true;
+		p = p->link;
+	}
+	return false;
+}
+bool LinkedList::Delete(string eno) // delete the element
+{
+	Node* p = first, * q = nullptr;
+	while (p != nullptr) {
+		if (p->data.eno == eno) {
+			if (p == first) {
+				first = p->link;
+				delete p;
+				return true;
+			}
+			q->link = p->link;
+			delete p;
+			return true;
+		}
+		q = p;
+		p = p->link;
+	}
+	return false;
+}
+LinkedList& LinkedList::operator+(LinkedList& lb) {
+	LinkedList lc;
+	Node* a = first, * b = lb.first;
+	while (a != nullptr && b != nullptr) {
+		if (a->data < b->data) {
+			lc.Add(a->data);
+			a = a->link;
+		}
+		else {
+			lc.Add(b->data);
+			b = b->link;
+		}
+	}
+	while (a != nullptr) {
+		lc.Add(a->data);
+		a = a->link;
+	}
+	while (b != nullptr) {
+		lc.Add(b->data);
+		b = b->link;
+	}
+	return lc;
+}
+
 bool ListIterator::NotNull() {
-	if (current != NULL)
+	if (current != nullptr)
 		return true;
-	else
-		return false;
+	return false;
 }
 bool ListIterator::NextNotNull() {
-	if (current->link != NULL)
+	if (current->link != nullptr)
 		return true;
-	else
-		return false;
+	return false;
 }
 Employee* ListIterator::First() {
 	return &list.first->data;
 }
 Employee* ListIterator::Next() {
-	Employee* e = &current->data;
 	current = current->link;
+	Employee* e = &current->data;
 	return e;
 }
 
@@ -197,12 +241,12 @@ bool ListIterator::operator != (const ListIterator right) const {
 bool ListIterator::operator == (const ListIterator right) const {
 	return current == right.current;
 }
-//int printAll(const List& l);//list iterator를 사용하여 작성하는 연습
-//int sumProductFifthElement(const List& l);//list iterator를 사용하여 작성하는 연습
+
 int sum(const LinkedList& l)
 {
-	int ret = 0;
 	ListIterator li(l);
+	if (!li.NotNull()) return 0; //리스트가 비어있는 경우
+	int ret = 0;
 	while (li.NotNull()) {
 		ret += li.GetCurrent()->getSalary();
 		li++;
@@ -213,41 +257,47 @@ int sum(const LinkedList& l)
 double avg(const LinkedList& l)
 {
 	ListIterator li(l);
-	double sum = 0, cnt = 0;
+	if (!li.NotNull()) return 0; //리스트가 비어있는 경우
+	int sum = 0, n = 0;
 	while (li.NotNull()) {
 		sum += li.GetCurrent()->getSalary();
-		cnt++;
+		n++;
 		li++;
 	}
-	return sum / cnt;
+	double ret = (double)sum / (double)n;
+	return ret;
 }
 
 int min(const LinkedList& l)
 {
 	ListIterator li(l);
-	int Min = 987654321;
+	if (!li.NotNull()) return 0; //리스트가 비어있는 경우
+	int min = 987654321;
 	while (li.NotNull()) {
-		if (li.GetCurrent()->getSalary() <Min)
-			Min = li.GetCurrent()->getSalary();
+		int salary = li.GetCurrent()->getSalary();
+		if (salary < min)
+			min = salary;
 		li++;
 	}
-	return Min;
+	return min;
 }
 
 int max(const LinkedList& l)
 {
 	ListIterator li(l);
-	int Max = 0;
+	if (!li.NotNull()) return 0; //리스트가 비어있는 경우
+	int max = 0;
 	while (li.NotNull()) {
-		if (li.GetCurrent()->getSalary() > Max)
-			Max = li.GetCurrent()->getSalary();
+		int salary = li.GetCurrent()->getSalary();
+		if (salary > max)
+			max = salary;
 		li++;
 	}
-	return Max;
+	return max;
 }
 
 enum Enum {
-	Add, Delete, Show, Search, SUM, AVG, MIN, MAX, Exit
+	Add0, Add1, Delete, Show, Search, Merge, SUM, AVG, MIN, MAX, Exit
 };
 
 int main() {
@@ -255,51 +305,65 @@ int main() {
 	int selectMenu, num;
 	string eno, ename;
 	int pay;
-	Employee data;
+	Employee* data;
 	bool result = false;
-	srand(time(NULL));
-	LinkedList* l = new LinkedList();
-
+	LinkedList la, lb, lc;
 	do {
-		cout << "0.ADD, 1.Delete, 2.Show, 3.Search, 4. sum, 5.avg, 6.min, 7.max, 8.exit" << endl << "선택::";
+		cout << "0.Add0, 1.Add1, 2.Delete, 3.Show, 4.Search, 5.Merge, 6. sum, 7.avg, 8.min, 9.max, 10.exit" << endl << "선택::";
 		cin >> selectMenu;
 		switch (static_cast<Enum>(selectMenu)) {
-		case Add:
+		case Add0:
 			cout << "사원번호 입력:: ";
 			cin >> eno;
 			cout << "사원 이름 입력:: ";
 			cin >> ename;
 			cout << "사원 급여:: ";
 			cin >> pay;
-			data = Employee(eno, ename, pay);
-			l->Add(data);
+			data = new Employee(eno, ename, pay);
+			la.Add(*data);
+			break;
+		case Add1:
+			cout << "사원번호 입력:: ";
+			cin >> eno;
+			cout << "사원 이름 입력:: ";
+			cin >> ename;
+			cout << "사원 급여:: ";
+			cin >> pay;
+			data = new Employee(eno, ename, pay);
+			lb.Add(*data);
 			break;
 		case Delete:
 			cout << "사원번호 입력:: ";
 			cin >> eno;
-			result = l->Delete(eno);
+			result = la.Delete(eno);
 			if (result)
 				cout << "삭제 완료";
 			break;
-		case Show: // 꼬리 노드 삭제
-			l->Show();
+		case Show:
+			cout << "리스트 la = ";
+			la.Show();
+			cout << "리스트 lb = ";
+			lb.Show();
 			break;
 		case Search:
 			cout << "사원번호 입력:: ";
 			cin >> eno;
-			result = l->Search(eno);
+			result = la.Search(eno);
 			if (!result)
 				cout << "검색 값 = " << eno << " 데이터가 없습니다." << endl;
 			else
 				cout << "검색 값 = " << eno << " 데이터가 존재합니다." << endl;
 			break;
-		case SUM:  cout << "sum() = " << sum(*l) << endl; break;
-		case AVG:  cout << "avg() = " << avg(*l) << endl; break;
-		case MIN:  cout << "min() = " << min(*l) << endl; break;
-		case MAX:  cout << "max() = " << max(*l) << endl; break;
-		case Exit: // 꼬리 노드 삭제
-			cout << "Exit";
-			exit(1);
+		case Merge:
+			lc = la + lb;
+			cout << "리스트 lc = ";
+			lc.Show();
+			break;
+		case SUM:  cout << "sum() = " << sum(la) << endl; break;
+		case AVG:  cout << "avg() = " << avg(la) << endl; break;
+		case MIN:  cout << "min() = " << min(la) << endl; break;
+		case MAX:  cout << "max() = " << max(la) << endl; break;
+		case Exit:
 			break;
 		}
 	} while (static_cast<Enum>(selectMenu) != Exit);
